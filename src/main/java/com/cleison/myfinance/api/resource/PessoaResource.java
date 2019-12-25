9bb2e50e792205e1c8b0e7d63f1fb15f92a5d455
Cleison Oliveira <cleisonv.oliveira@gmail.com>
2019-12-25 19:41:16 -0300

    Inclusao da classe de Servico e do verbo PUT
    Inclusao da classe de Servico e do verbo PUT

--- src/main/java/com/cleison/myfinance/api/resource/PessoaResource.java
+++ src/main/java/com/cleison/myfinance/api/resource/PessoaResource.java
@@ -5,20 +5,26 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cleison.myfinance.api.event.RecursoCriadoEvent;
 import com.cleison.myfinance.api.model.Pessoa;
 import com.cleison.myfinance.api.repository.PessoaRepository;
+import com.cleison.myfinance.api.service.PessoaService;
 
 @RequestMapping("/pessoas")
 @RestController
@@ -26,6 +32,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -51,5 +60,19 @@ public class PessoaResource {
 	public List<Pessoa> buscarPessoa(){
 		
 		return pessoaRepository.findAll();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+		
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		
+		return ResponseEntity.ok(pessoaSalva);
 	}
 }

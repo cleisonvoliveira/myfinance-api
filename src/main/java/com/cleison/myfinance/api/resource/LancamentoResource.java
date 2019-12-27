5086b0d41ba9c7b6ce4fa1daa460212425c485bc
Cleison Oliveira <cleisonv.oliveira@gmail.com>
2019-12-26 21:52:09 -0300

    Implentação do Cadastro de Lancamentos
    * Inclusão do tratamento de exceção "DataIntegrityViolationException";
    * Inclusão da dependência "commons-lang3" para auxiliar a pegar o rootCause da exceção;
    *  Implementação do cadastrado de Lancamentos.

--- src/main/java/com/cleison/myfinance/api/resource/LancamentoResource.java
+++ src/main/java/com/cleison/myfinance/api/resource/LancamentoResource.java
@@ -2,13 +2,22 @@ package com.cleison.myfinance.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cleison.myfinance.api.event.RecursoCriadoEvent;
 import com.cleison.myfinance.api.model.Lancamento;
 import com.cleison.myfinance.api.repository.LancamentoRepository;
 
@@ -18,6 +27,9 @@ public class LancamentoResource {
 	
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Lancamento> buscarLancamentos(){
@@ -27,6 +39,13 @@ public class LancamentoResource {
 	@GetMapping("/{codigo}")
 	public Lancamento buscarPeloCodigo(@PathVariable Long codigo){
 		return lancamentoRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criarLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 	
 }

2109d2e6ddfe0f00d28d15f48764d1588afe5e8a
Cleison Oliveira <cleisonv.oliveira@gmail.com>
2020-01-01 21:33:31 -0300

    Alterando para usar Access Token Jwt
    Alterando para usar Access Token Jwt

--- src/main/java/com/cleison/myfinance/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/cleison/myfinance/api/config/AuthorizationServerConfig.java
@@ -8,8 +8,11 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -32,11 +35,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 			.tokenStore(tokenStore())//armazenando o token
+			.accessTokenConverter(accessTokenConverter())
 			.authenticationManager(authenticationManager);
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessToken = new JwtAccessTokenConverter();
+		accessToken.setSigningKey("MyFinanceAPI");
+		return accessToken;
 	}
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+//		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
